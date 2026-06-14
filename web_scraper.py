d761b3074a0b2ef49823d5919129406be0d0d31d
Ecl9pse <tylermcm98@gmail.com>
2026-06-13 19:05:28 -0500

    Basic Single Page Webscraper

---
+++ web_scraper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+def scrapePost():
+    pageURL = "https://www.warhammer-community.com/en-gb/articles/hjdxp86f/saturday-pre-orders-warhammer-40000-armageddon/"
+    page = requests.get(pageURL)
+
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    products = soup.find_all("span", attrs = {"class":"font-bold"})
+    date = soup.find("time", attrs = {"class":"copy-bitter-xs whitespace-nowrap hidden @[175px]/video:block @[225px]/article:block xl:!block"})
+
+    print(date.text)
+    for product in products:
+        print(product.text)
+    
+
+scrapePost()
+
+    
