4fc957ac81b211c805eddef3b0311fd292b577e5
Ecl9pse <tylermcm98@gmail.com>
2026-06-15 00:27:54 -0500

    update url scanner

--- web_scraper.py
+++ web_scraper.py
@@ -1,12 +1,18 @@
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from playwright.sync_api import sync_playwright
 from pymongo import MongoClient
 import requests
 import json
 import os
 
 def scrapePost():
-    pageURL = "https://www.warhammer-community.com/en-gb/articles/hjdxp86f/saturday-pre-orders-warhammer-40000-armageddon/"
+    pageURL = getURL()
+
+    if not pageURL:
+        print("Error: Failed to get URL")
+        return
+
     page = requests.get(pageURL)
 
     soup = BeautifulSoup(page.text, "html.parser")
@@ -33,8 +39,29 @@ def scrapePost():
 
     collection.insert_one(data)
 
+def getURL():
+    baseURL = "https://www.warhammer-community.com"
+    whComURL = "https://www.warhammer-community.com/en-gb/topics/pre-orders/"
 
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(whComURL)
+
+        page.wait_for_selector("article", timeout=10000)
+        
+        html = page.content()
+        browser.close()
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    links = soup.find_all("a", class_=["btn-cover", "mb-15", "md:mb-20", "block", "link-underline", "link-underline--light", "text-grimDarkBlack"])
     
+    for link in links:
+        if link["href"].startswith("/en-gb/articles/"):
+            return baseURL + link["href"]
+
+
 load_dotenv()
 scrapePost()
 
